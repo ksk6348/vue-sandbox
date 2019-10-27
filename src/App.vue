2481d8ff8ff412155bc6fec7dee572027c856fdb
ohya <ksk6348@gmail.com>
2019-10-27 21:35:26 +0900

    class component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" :val="value"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'app',
-  data: () => ({
-    value: 123,
-  }),
+@Component({
   components: {
     HelloWorld,
   },
-};
+})
+export default class App extends Vue {}
 </script>
 
 <style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,22 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>{{ message }}</h2>
-    <h3>{{ value }}</h3>
+    <h2>{{ message() }}</h2>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-    val: [Number, String],
-  },
-  computed: {
-    message(): string {
-      return this.msg;
-    },
-    value(): string | number {
-      return this.val;
-    },
-  },
-};
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop()
+  private msg: string;
+
+  private message(): string {
+    return this.msg;
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
